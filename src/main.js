336667154977bf85cd85976dfbf99354b7b82ab6
111Makss111 <777docmax777@gmail.com>
2025-02-06 16:32:46 +0100

    і

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-import { fetchPhotosByQuery } from "./js/pixabay-api";
-import { renderImages } from "./js/render-functions";
+import { fetchPhotosByQuery } from './js/pixabay-api';
+import { renderImages } from './js/render-functions';
 
 const form = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search-images');
 const loader = document.querySelector('.loader');
 
 document.addEventListener('DOMContentLoaded', () => {
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
     const value = searchInput.value.trim();
     if (value === '') {
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
       .then(data => {
         if (data.hits.length === 0) {
           iziToast.error({
-            title: 'Error!',
             message:
               'Sorry, there are no images matching your search query. Please try again!',
             position: 'topRight',
@@ -35,9 +34,10 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch(error => {
         console.error('Error fetching images:', error);
         throw error;
-      }).finally(() => {
-      loader.style.display = 'none';
-    });
+      })
+      .finally(() => {
+        loader.style.display = 'none';
+      });
     searchInput.value = '';
   });
 });
